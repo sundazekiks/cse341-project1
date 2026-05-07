81519f2eee1e372b62462194bffaa848cf5cfd0d
sundazekiks <abutontravis1@gmail.com>
2026-05-07 16:40:11 -0400

    try

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -7,6 +7,7 @@ const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
+    tlsAllowInvalidCertificates: true,
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
